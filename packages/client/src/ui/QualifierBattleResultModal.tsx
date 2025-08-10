d5deda6d758cdb0243885af942fccbffa14061ef
insraq <ruoysun@gmail.com>
2025-08-10 22:41:19 +0300

    [build]

--- packages/client/src/ui/QualifierBattleResultModal.tsx
+++ packages/client/src/ui/QualifierBattleResultModal.tsx
@@ -1,3 +1,4 @@
+import { Boosters } from "@spaceship-idle/shared/src/game/definitions/Boosters";
 import { GameState, GameStateUpdated } from "@spaceship-idle/shared/src/game/GameState";
 import { BattleStatus } from "@spaceship-idle/shared/src/game/logic/BattleStatus";
 import { BattleType } from "@spaceship-idle/shared/src/game/logic/BattleType";
@@ -9,15 +10,23 @@ import { hideModal } from "../utils/ToggleModal";
 import { BattleReportComp } from "./BattleReportComp";
 import { DefeatedHeaderComp, VictoryHeaderComp } from "./components/BattleResultHeader";
 import { hideLoading, showLoading } from "./components/LoadingComp";
+import { TextureComp } from "./components/TextureComp";
 
 export function QualifierBattleResultModal(): React.ReactNode {
    const win = G.runtime.battleStatus === BattleStatus.LeftWin;
    const booster = rollBooster(G.save.current);
-   console.log(booster);
    return (
       <div className="m10">
          {win ? <VictoryHeaderComp /> : <DefeatedHeaderComp />}
-         <div className="panel"></div>
+         <div className="panel">
+            {booster ? (
+               <div className="row">
+                  <TextureComp name={`Booster/${booster}`} width={32} />
+                  <div className="f1">{Boosters[booster].name()}</div>
+                  <div>+1</div>
+               </div>
+            ) : null}
+         </div>
          <div className="h5" />
          <BattleReportComp />
          <div className="h10" />
